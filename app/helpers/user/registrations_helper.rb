ece5ee083f3c41bbb01bc562f43399efc84cba74
gjack <gabi@gabijack.com>
2017-12-14 14:00:08 -0500

    Display account creation errors

--- app/helpers/user/registrations_helper.rb
+++ app/helpers/user/registrations_helper.rb
@@ -1,7 +1,19 @@
 module User::RegistrationsHelper
   def sign_up_errors
     if @sign_up.errors.any?
-      @sign_up.errors.inspect
+      messages = @sign_up.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+      sentence = I18n.t("errors.messages.not_saved",
+        :count => @sign_up.errors.count,
+        :resource => "account")
+
+      html = <<-HTML
+      <div class="alert alert-warning">
+        <h2>#{sentence}</h2>
+        <ul>#{messages}</ul>
+      </div>
+      HTML
+
+      html.html_safe
     end
   end
 end
