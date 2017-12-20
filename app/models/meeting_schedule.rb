d4e88a5bba7d30b3ec85472555576a3b5e25c840
gjack <gabi@gabijack.com>
2017-12-20 12:11:56 -0500

    Create upcoming and next meetings as needed

--- app/models/meeting_schedule.rb
+++ app/models/meeting_schedule.rb
@@ -8,12 +8,12 @@ class MeetingSchedule < ApplicationRecord
 
   def upcoming_meeting
     meeting_date = schedule.next_occurrence(Time.now.beginning_of_day - 1.day)
-    meetings.find_by(date: meeting_date)
+    meeting_for_date(meeting_date)
   end
 
   def next_meeting
     meeting_date = schedule.next_occurrence
-    meetings.find_by(date: meeting_date)
+    meeting_for_date(meeting_date)
   end
 
   def previous_meeting
@@ -49,5 +49,15 @@ class MeetingSchedule < ApplicationRecord
         organization_id: team_member.organization_id,
         })
     end
+  end
+
+  def meeting_for_date(meeting_date)
+    meeting = meetings.find_or_initialize_by(date: meeting_date)
+    if meeting.new_record?
+      meeting.team_member_id = team_member_id
+      meeting.organization_id = team_member.organization.id
+      meeting.save
+    end
+    meeting
   end
 end
